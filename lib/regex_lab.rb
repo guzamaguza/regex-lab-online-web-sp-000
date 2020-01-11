435d544afddfe0c4d9c17b9e6e06dc2ed4895c22
Mark A Guza <guzamaguza@gmail.com>
2020-01-11 03:00:00 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,14 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[aeiouAEIOU]\w+/)
+    return true
+  elsif word.match(/\A[^aeiou]/)
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/\bun\w+ing\b/)
+    #return text.scan(/[^un][ing$]^[?!pomp]/)
 end
 
 def words_five_letters_long(text)
