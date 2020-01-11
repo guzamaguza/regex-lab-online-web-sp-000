91c5d321285c7a3f2bdcd3adc52f253475086f96
Mark A Guza <guzamaguza@gmail.com>
2020-01-11 03:50:42 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-    match.text(/^[A-Z].*[a-zA-Z][]$/) ? true : false
+    match.text(/^[A-Z].*[a-zA-Z][.]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
