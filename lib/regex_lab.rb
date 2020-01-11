ab0ea252c9ea48049f932d625b90759415496708
Mark A Guza <guzamaguza@gmail.com>
2020-01-11 03:29:52 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,11 +8,10 @@ end
 
 def words_starting_with_un_and_ending_with_ing(text)
     text.scan(/\bun\w+ing\b/)
-    #return text.scan(/[^un][ing$]^[?!pomp]/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
