f2b00b5dde83ea04e59561fa67b33e98e1d41db2
Mark A Guza <guzamaguza@gmail.com>
2020-01-11 04:24:11 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,5 +19,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-    phone.match(/^[ (]?[1-9]{3}[-. )]?[1-9]{3}[- ]?[1-9]{4}$/) ? true : false
+    phone.match(/^[ (]?[1-9]{3}[-. )]?[1-9]{3}[- ]?[1-9]{4}/) ? true : false
 end
